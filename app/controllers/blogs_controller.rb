5f8691c6f3888f74a49e912ea5374835617ede6c
JarbiBonilla <jarbibonilla@gmail.com>
2020-07-29 18:30:18 -0400

    created show and create actions in blogs controller for showing and creating new blogs and rendering them as json

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,8 +1,24 @@
 class BlogsController < ApplicationController
 
+    before_action :set_blog, only: [:show, :update, :destroy]
+
     def index
         @blogs = Blog.all
         
         render json: @blogs
     end
+
+    def show
+        render json: @blog 
+    end
+
+    def create
+        @ablog = Blog.new(blog_params)
+        if @blog.save
+            render json: @blog 
+        else
+            render json: @blog.errors
+        end
+    end
+    
 end
