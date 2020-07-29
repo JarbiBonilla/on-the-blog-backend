2f2d1a6818b6fab6ba1dcb8d5db0c07376117a5d
JarbiBonilla <jarbibonilla@gmail.com>
2020-07-29 18:58:28 -0400

    created seed data and tested the json in the browser

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Blog.create([{
+        title: "First Blog Post!",
+        content: "No matter how beautiful the sunset, it saddened her knowing she was one day older.
+        Everyone was curious about the large white blimp that appeared overnight.
+        The ants enjoyed the barbecue more than the family.
+        He picked up trash in his spare time to dump in his neighbor's yard.
+        There were three sphered rocks congregating in a cubed room.",
+        link: "example",
+        favorite: 0
+    },
+
+    {
+        title: "Second Blog Post!",
+        content: "The hand sanitizer was actually clear glue.
+        This is a Japanese doll.
+        The river stole the gods.
+        Going from child, to childish, to childlike is only a matter of time.
+        As you consider all the possible ways to improve yourself and the world, you notice John Travolta seems fairly unhappy.",
+        link: "example2",
+        favorite: 0
+    },
+
+    {
+        title: "Third Blog Post!",
+        content: "She did not cheat on the test, for it was not the right thing to do.
+        The secret ingredient to his wonderful life was crime.
+        Fluffy pink unicorns are a popular status symbol among macho men.
+        He shaved the peach to prove a point.
+        The teens wondered what was kept in the red shed on the far edge of the school grounds.",
+        link: "example3",
+        favorite: 0
+    }
+
+])
